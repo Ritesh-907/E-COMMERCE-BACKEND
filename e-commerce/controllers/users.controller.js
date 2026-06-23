922a26c0f6ef86acc76b373669816f0eaa6f3041
Ritesh-907 <chaudharyritesh.7777@gmail.com>
2026-06-23 20:15:42 +0530

    remove all existing application files and controllers

--- e-commerce/controllers/users.controller.js
+++
@@ -1,8 +0,0 @@
-exports.getAllCustomer = (req, res, next) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      name: "Ritesh",
-    },
-  });
-};
--- e-commerce/router/users.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const usersController = require("../controllers/users.controller");
-router.route("/customer").get(usersController.getAllCustomer);
-
-module.exports = router;
